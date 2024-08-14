ec6ac1a547b2ff014b31a1fc81d3941a258fd065
unknown <cryptonativecoding@gmail.com>
2024-08-15 02:51:01 +0700

    5 commit Firebase Login

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -3,15 +3,34 @@ import { getAuth } from "firebase/auth";
 
 
 // create .env on the root folder and fill with your firebase credential
+// const firebaseConfig = {
+// 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+// 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+// 	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+// 	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+// 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+// 	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// 	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+
+	apiKey: "AIzaSyCnYx9H1yBdnl1_yHDVeH3NWEEzRhZonVY",
+  
+	authDomain: "sate-patriot.firebaseapp.com",
+  
+	projectId: "sate-patriot",
+  
+	storageBucket: "sate-patriot.appspot.com",
+  
+	messagingSenderId: "485248563641",
+  
+	appId: "1:485248563641:web:62b7418cd111c3895ede68"
+  
+  };
+  
+  
+
 
 const app = initializeApp(firebaseConfig);
 
