e7ebd93057ab9aa1de83e7baeb52203f16454506
unknown <cryptonativecoding@gmail.com>
2024-09-04 05:33:30 +0700

    commit 12 Map picker

--- src/components/map/MapPicker.js
+++ src/components/map/MapPicker.js
@@ -20,10 +20,10 @@ const App = () => {
 
 
   
-// const handleChangeLocationBtn = () => {
-//   dispatch(setShowMap(true)) 
-//   dispatch(setModalMapPicker(false))
-// }
+const handleChangeLocationBtn = () => {
+//  dispatch(setShowMap(true)) 
+  dispatch(setModalMapPicker(false))
+}
 
   function handleChangeLocation (lat, lng){ 
   
@@ -78,9 +78,9 @@ const App = () => {
 
    </section>
 
-   {/* <button onClick={handleChangeLocationBtn} className="w-full mt-4  text-white bg-orange-700 hover:bg-blue-800 
+   <button onClick={handleChangeLocationBtn} className="w-full mt-4  text-white bg-orange-700 hover:bg-blue-800 
   focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 
-  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">  SIMPAN LOKASI</button> */}
+  text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">  SIMPAN LOKASI</button>
   </> 
   );
 }
